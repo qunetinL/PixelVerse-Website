70d82fc601812c3c67a03b7e4f359da84d6dc882
quentin452 <catzworld.contact@gmail.com>
2026-02-12 12:51:32 +0100

    Fix main card responsiveness

--- src/Views/layout/main.php
+++ src/Views/layout/main.php
@@ -7,7 +7,7 @@
     <title>
         <?php echo $title ?? 'PixelVerse Studios'; ?>
     </title>
-    <link rel="stylesheet" href="/css/style.css?v=1.1">
+    <link rel="stylesheet" href="/css/style.css?v=1.2">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
